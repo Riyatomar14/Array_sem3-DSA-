48be80eaef36d60b8618e075832e1b683376d667
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-09-18 12:33:29 +0530

    Create practise.cpp

---
+++ practise.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+bool search(int arr[],int key,int n){
+    for(int i=0; i<n ;i++){
+        arr[i]=key;
+        break;
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cout << "write the size of the array you want:";
+    cin >> n;
+    int arr[n];
+    cout<< " the array is ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout << arr[i];
+    }
+    //reverse the array
+    cout<<endl;
+    for(int i=n-1;i>=0;i--){
+        cout << arr[i];
+    }
+    
+    // search the element
+    int key;34
+    cout << endl;
+    cout << "enter the number you want to search:";
+    cin >> key;
+    search(arr,key,n);
+    if (search) {
+        cout << "key is present";
+    }
+    else {
+        cout << "key is not present";
+    }
+    
+    
+    
+    return 0;
+}
